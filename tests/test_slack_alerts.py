a6f6d968b0694a96c5c5e66d0d65167f68bca845
etienne-napoleone <etienne@tomochain.com>
2018-11-28 21:27:08 +0700

    test: add tests for getting and setting an alerter

--- tests/test_slack_alerts.py
+++ tests/test_slack_alerts.py
@@ -1,5 +1,19 @@
+import pytest
+
+import slack_alerts as sa
 from slack_alerts import __version__
 
 
 def test_version():
     assert __version__ == '0.1.0'
+
+
+def test_get_alerter_ok():
+    sa.alerter('https://slack.com/kek', '#test', name='test')
+    assert sa.get_alerter('test')
+    del sa._alerters['test']
+
+
+def test_get_alerter_nok():
+    with pytest.raises(sa.NoSuchAlerter):
+        sa.get_alerter('test')
